2de1c89db67d000b64ddf72e19bb7c267d5c1d29
Tarunakumar Sundaray <tarunakumar.sundaray@int.whishworks.com>
2019-11-21 12:39:26 +0530

    POM file changes14

--- src/test/java/com/training/testcase/MySQLTest.java
+++ src/test/java/com/training/testcase/MySQLTest.java
@@ -7,11 +7,23 @@ import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class MySQLTest extends FunctionalTestCase{
-	@Test
+	/*@Test
 	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-		MuleEvent event = runFlow("mysql-mavenFlow");
+		MuleEvent event = runFlow("junitflow");
 		String contentType = event.getMessage().getOutboundProperty("Content-Type");
 		assertEquals("application/json", contentType);
-	}
+	}*/
+	
+	   @Test
+	    public void mavenFlowReturnsHelloMaven() throws Exception {
+	        runFlowAndExpect("junitflow", "Hello Maven");
+	    }
+	
+	   // run maven-config.xml when testing
+    @Override
+    protected String getConfigFile(){
+      return "mysql-maven.xml";
+    }
+
 
 }
